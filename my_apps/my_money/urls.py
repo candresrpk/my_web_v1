ae89f7aab3f9c45c9ac7df3fae13f78c6c1fb4df
Carlos Reyes <andrespk3000@gmail.com>
2025-10-12 21:07:11 -0500

    dashboard initiated, sidebar created

--- my_apps/my_money/urls.py
+++ my_apps/my_money/urls.py
@@ -6,4 +6,6 @@ app_name = 'my_money'
 
 urlpatterns = [
     path('', views.HomeView, name='home'),
+    path('dashboard/', views.My_money_dashboardView, name='dashboard'),
+    path('create_cartera/', views.CreateCarteraView, name='create_cartera'),
 ]
--- my_apps/my_money/views.py
+++ my_apps/my_money/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from .forms import CarteraForm, CategoriaForm, MovimientoForm
 
 # Create your views here.
 
 
 def HomeView(request):
-    return render(request, 'my_money/home.html')
+    
+    if request.user.is_authenticated:
+        return redirect('my_money:dashboard')
+    else:
+        messages.info(request, 'Por favor, inicia sesión para continuar a la aplicación My Money')
+        return redirect('accounts:login')
+    
+
+def My_money_dashboardView(request):
+    return render(request, 'my_money/my_money_dashboard.html')
 
 
 def CreateCarteraView(request):
